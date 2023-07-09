872762ff709b97f64401550c27a110952649d181
Mikhei1i <132217491+MakerM1@users.noreply.github.com>
2023-07-09 18:05:21 +0400

    v2

--- scripts/11Todo-script.js
+++ scripts/11Todo-script.js
@@ -1,28 +1,25 @@
-let todoList = JSON.parse(localStorage.getItem('memory')) || []
+const todoList = JSON.parse(localStorage.getItem('memory')) || []
 
 renderTodoList();
 
 function renderTodoList() {
     let todoListHTML = '';
 
-for (let i = 0; i < todoList.length; i++) {
-    const todoObject = todoList[i];
-    // const name = todoObject.name;
-    // const dueDate = todoObject.dueDate;
-    const { name, dueDate } = todoObject;
-    const html = `
-    <div class="row-box">
-    <div class="name-box">${name}</div> 
-    <div class="date-box">${dueDate}</div>
-   <button onclick="
-    todoList.splice(${i}, 1);
-    renderTodoList(); saveToDoList();
-    " class="delete-button">Delete</button>
-    </div>
-    `
-    todoListHTML += html
-}
+    todoList.forEach(function(todoObject, index) {
+        let { name, dueDate } = todoObject;
+        const html = `
+        <div class="row-box">
+        <div class="name-box">${name}</div> 
+        <div class="date-box">${dueDate}</div>
+       <button onclick="
+        todoList.splice(${index}, 1);
+        renderTodoList(); saveToDoList();
+        " class="delete-button">Delete</button>
+        </div>
+        `
+        todoListHTML += html
 
+    });
 
 document.querySelector('.js-todo-div').innerHTML = todoListHTML
 }
